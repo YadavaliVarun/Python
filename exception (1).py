0e0a42e247caaed33e081f7819f530ee744dcc39
YadavaliVarun <167081811+YadavaliVarun@users.noreply.github.com>
2024-04-18 21:50:32 +0530

    Add files via upload

---
+++ exception (1).py
@@ -0,0 +1,15 @@
+print("starting line")
+a = [11, 22, 33]
+print(a)
+
+print(a[1])
+#try:
+    #a= 10 / 0
+    #print(a)
+#except:
+        #print("Some exception raised")
+#else:
+   # print("No exception raised,everything worked")
+#finally:
+    #print("This is a final block")
+#print("Outside try block")
